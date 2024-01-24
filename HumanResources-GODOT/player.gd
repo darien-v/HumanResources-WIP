589e86be82c9a3b0b337449ca32f9a28993384a4
darien-v <darienvazquez.cs@gmail.com>
2024-01-23 19:04:17 -0500

    added source files to repo

---
+++ HumanResources-GODOT/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody3D
+
+# player speed in m/s
+@export var speed = 14
+# downward acceleration in m/s
+@export var fall_acceleration = 75
+# jumping (vertical) impulse in m/s
+@export var jump_impulse = 20
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	# input direction
+	var direction = Vector3.ZERO
+
+	# check for each move input + update direction
+	# XZ plane = ground
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	
+	# normalize the direction vector
+	# otherwise it'll go faster on diagonals (2 keys pressed)
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(position + direction, Vector3.UP)
+	
+	# ground velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# vertical velocity (fall after jump)
+	if not is_on_floor(): # gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# vertical velocity (jumping up)
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+		
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
